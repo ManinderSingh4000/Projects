a9181a2da7c58e7b870ad129f4cc83bf9d510583
Maninder Singh <99421094+ManinderSingh4000@users.noreply.github.com>
2025-04-21 12:10:05 +0530

    Update myappie.py

--- myappie.py
+++ myappie.py
@@ -310,29 +310,6 @@ if file is not None:
                      plot_tree(model_eval, filled=True, fontsize=8)
                      st.pyplot(fig_eval)
 
-                # elif model_selection == "Random Forest":
-                #     model_eval = RandomForestClassifier()
-                #     model_eval.fit(X_transformed_eval, y_eval)
-                #     y_pred_eval = model_eval.predict(X_transformed_eval)
-                #     cm_eval = confusion_matrix(y_eval, y_pred_eval)
-                #     fig_eval, ax_eval = plt.subplots()
-                #     sns.heatmap(cm_eval, annot=True, fmt='d', cmap='Blues')
-                #     st.pyplot(fig_eval)
-
-                elif model_selection == "Random Forest":
-                    k_estimators = st.slider("Select number of clusters (k)", 2, 10 )
-                    model_eval = RandomForestClassifier(n_estimators=k_estimators)
-                    model_eval.fit(X_transformed ,y_eval)
-
-                    # n_estimators_view = min(5, model_eval.n_estimators)  # Show a maximum of 5 trees
-                    st.subheader(f"First {n_estimators_view} Trees in the Random Forest")
-                    # fig_col = st.columns(n_estimators_view)
-                    for i in range(model_eval.estimators_):
-                        with fig_col[i]:
-                            st.subheader(f"Tree {i+1}")
-                            fig_tree, ax_tree = plt.subplots(figsize=(10, 5))
-                            plot_tree(model_eval.estimators_[i], filled=True, fontsize=6, feature_names=X_eval.columns) # Use original feature names if possible
-                            st.pyplot(fig_tree)
 
                 elif model_selection == "SVM":
                     # SVM decision region visualization supports exactly 2 features
